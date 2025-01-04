b23ce2942fd1c2a3e628b6e4e0d76dbd524399c5
User <thivyansivananthan@gmail.com>
2025-01-04 12:28:01 +0100

    100 Datensätze und anpassung Layout

--- StreamLitDemo/main.py
+++ StreamLitDemo/main.py
@@ -14,11 +14,23 @@ import ast
 import json
 import re
 import io
+import random
+
+
+st.set_page_config(
+    page_title="Streamlit Layouts Tutorial",
+    page_icon=":robot_face:",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+
+
 
 if "llm" not in st.session_state:
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
     st.session_state.llm = LlamaCpp(
-        model_path="./Assets/models/Meta-Llama-3.1-8B-Instruct-Q4_K_L.gguf",
+        model_path="./Assets/models/llama-3.2-3b-instruct-q8_0.gguf",
         callback_manager=callback_manager,
         n_ctx=4096,
         verbose=True,
@@ -44,7 +56,8 @@ def show_title_image():
     with middle_col:
         logo_header = config["headers"]["logo_header"]
         st.image("./Assets/static/images/front_logo.png", caption=logo_header)
-
+    with left_col:
+        logo_header = config["headers"]["logo_header"]
 
 def show_selectbox_and_return_selection():
     select_box_caption = config["labels"]["select_box_label"]
@@ -86,6 +99,91 @@ def clean_schema(schema_content):
 
     cleaned_schema = " ".join(cleaned_schema)
     return cleaned_schema
+
+def website_layout():
+    col = st.columns([1,3,1]) # 0.2 0.6 0.2
+    container_head = st.container(border=True)
+    st.divider()
+    container_body = st.container()
+    with container_head:
+        with col[0]:
+            st.image("./Assets/static/images/front_logo.png", width=200)
+
+        with col[1]:
+            search_input = st.text_input("Searching....",placeholder = "Enter product name",disabled=False)
+
+        with col[2]:
+            st.markdown("<style>div.stButton {margin-top: 14px;}</style>", unsafe_allow_html=True)
+            search_button = st.button("Submit",on_click=execute_query)
+            if(search_button):
+                st.session_state.random_zahl = random.randint(0, 100)
+
+    with container_body:
+        col = st.columns([1,2,2])  
+        container_search_1 = st.container(border=True)
+        
+        with col[0]:
+            st.write("### Optional Filters")
+            st.checkbox("Filter 1")
+            st.checkbox("Filter 2")
+            st.checkbox("Filter 3")
+
+
+        with container_search_1:
+            with col[1]:
+                st.markdown(
+                    """
+                    <div style="border: 2px solid #f0f0f0; padding: 10px; border-radius: 5px;">
+                        <h4>{}</h4>
+                    """.format(list[(st.session_state.random_zahl%2 == 1)]),
+                    unsafe_allow_html=True
+                )
+                
+
+
+        with col[2]:
+            st.markdown(
+                    """
+                    <div style="border: 2px solid #f0f0f0; padding: 10px; border-radius: 5px;">
+                        <h4>{}</h4>
+                    </div>
+                    <br>
+                    """.format(list[(st.session_state.random_zahl%2 == 0)]),
+                    unsafe_allow_html=True
+            )
+            for dog in dogs_data["dogs"]:
+                # Darstellung der Hundedaten
+                st.markdown(f"### {dog['breed']} ({dog['category']})")
+                if "image" in dog:
+                        st.image(dog["image"], caption=dog["breed"])
+                st.write(f"**Preis:** {dog['price']}")
+                st.write(f"**Geschlecht:** {dog['gender']}")
+                st.write(f"**Geburtsdatum:** {dog.get('birthMonth', 'Unbekannt')}")
+                #st.write(f"**Beschreibung:** {dog['description'][:150]}...")
+                
+                if "features" in dog:
+                    with st.expander(f"Features zu {dog['breed']}"):
+                        st.write("**Besondere Merkmale:**")
+                        feature_html = ""
+                        for feature in dog["features"]:
+                            feature_html += f"""
+                            <span style="
+                                display: inline-block;
+                                background-color: #f0f0f0;
+                                border: 1px solid #ddd;
+                                border-radius: 5px;
+                                padding: 5px 10px;
+                                margin: 5px;
+                                font-size: 14px;
+                                color: #333;
+                                font-weight: bold;
+                            ">{feature}</span>
+                            """
+                        st.markdown(feature_html, unsafe_allow_html=True)
+
+                with st.expander(f"Mehr zu {dog['breed']}"):
+                    st.markdown(f"#### Details zu {dog['breed']}")
+                    st.write(dog["description"])
 
 
 def execute_query():
@@ -146,19 +244,27 @@ def execute_query():
 config = configparser.ConfigParser()
 config.read('./Assets/static/strings/labels.ini')
 
-show_title_image()
+if "random_zahl" not in st.session_state:
+    st.session_state.random_zahl = random.randint(0, 100)
 
 db_labels = os.listdir("./Assets/datasets/spider_data/database/")
 if ".DS_Store" in db_labels:
     db_labels.remove(".DS_Store")
 
+
+with open("./Assets/static/database_dog.json", "r", encoding="utf-8") as f:
+    dogs_data = json.load(f)
+
+website_layout()
+
+list = ["Search-Results with static Template", "Search-Results with SQI"]
+
+st.write(len(dogs_data["dogs"]))
+
 seletected_database = show_selectbox_and_return_selection()
 
 show_selected_db_schema()
 
-search_input_label = config["labels"]["search_input_label"]
-search_input = st.text_input(label=search_input_label)
-search_button_submit = st.button(label="Sumbit", on_click=execute_query)
 
 if "result_query" in st.session_state:
     st.header("_Generated_ Repsonse by :red[LLM]", divider="blue")
