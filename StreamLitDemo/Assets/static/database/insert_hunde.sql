7bfff9463240fa3fd05da275857409157f6da1a1
jaort103 <jacob@ortenberg.com>
2025-01-14 00:09:37 +0100

    anpassen des geänderten jpeg-file namens im code

--- StreamLitDemo/Assets/static/database/insert_hunde.sql
+++ StreamLitDemo/Assets/static/database/insert_hunde.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:544f9ce1f6cf7f637a1716be51f218ddb65d35e1a83d6bb077f8760eea049868
-size 503941
+oid sha256:7d254580044c5c5f9c4bbcb435839d18a427274f5e3d84de80d7897a898abb3e
+size 503809
